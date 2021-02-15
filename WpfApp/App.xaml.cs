da6c1f98233b0048bb0f66bb04770b08bd1e8188
bymagka <bymagka@bk.ru>
2021-02-15 19:50:32 +0300

    wpfApp

---
+++ WpfApp/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+    public class User
+    {
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
